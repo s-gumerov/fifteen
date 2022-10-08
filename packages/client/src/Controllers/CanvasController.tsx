75f08fce3a6972e80365b1b6a25e51cf588d1853
Evgeniy <turkin-e.v@yandex.ru>
2022-10-08 17:50:09 +0400

    task is done

---
+++ packages/client/src/Controllers/CanvasController.tsx
@@ -0,0 +1,109 @@
+// Отрисовка игрового поля
+const drawField = (ctx: CanvasRenderingContext2D, board: number[][], bg: HTMLImageElement): void => {
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      let dx = 0;
+      if (board && board[i][j] > 9) {
+        dx= 40;
+      } else {
+        dx = 0;
+      };
+      if (ctx) {
+        ctx.font = "128px serif";
+        ctx.fillStyle = '#F5F5F5';
+        ctx.fillRect(j * 200, i * 200, 210, 210);
+        if (board && board[i][j] === 0) {
+          ctx.clearRect((5 + j * 200), (5 + i * 200), 200, 200);
+        } else {
+          ctx.drawImage(bg, (5 + j * 200), (5 + i * 200));
+          ctx.fillText(`${board[i][j]}`, 65 - dx + 200 * j, 150 + 200 * i);
+        }
+      }
+    }
+  } 
+};
+
+// Получаем позицию пустой ячейки
+const getEmptyCell = (board: number[][]): {xEmptyCell: number, yEmptyCell: number} | undefined => {
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (board[j][i] === 0) {
+        return {
+          xEmptyCell: j,
+          yEmptyCell: i
+        };
+      }
+    }
+  }
+};
+
+// Перемешиваем случайным образом пятнашки на поле
+const mixBoard = (board: number[]): number[][] | undefined => {
+  board.sort(() => Math.random() - 0.5);
+  const newBoard = [];
+  for (let i = 0; i < 4; i++) {
+    const subArray = []
+    for(let j = i * 4 + 0; j < (i + 1) * 4; j++) {
+      subArray.push(board[j]);
+    }
+    newBoard.push(subArray);
+  }
+  return newBoard;
+};
+
+// Проверка победы
+const isWin = (board: number[][]): boolean => {
+  const winBoard = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 0]
+  ];
+
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      if (board[i][j] != winBoard[i][j]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+// Перемещаем пятнашку на пустое место
+const movePuzzleItem = (board: number[][], yMouse: number, xMouse: number): {board: number[][], stepsIsDone: boolean} => {
+  const emptyCell = getEmptyCell(board);
+  let stepsIsDone = false;
+  if (emptyCell) {
+    const {xEmptyCell, yEmptyCell} = emptyCell; 
+    if (xEmptyCell === xMouse && (yEmptyCell === yMouse - 1 || yEmptyCell === yMouse + 1)) {
+      board[xEmptyCell][yEmptyCell] = board[xMouse][yMouse];
+      board[xMouse][yMouse] = 0;
+      stepsIsDone = true;
+    };
+    if (yEmptyCell === yMouse && (xEmptyCell === xMouse - 1 || xEmptyCell === xMouse + 1)) {
+      board[xEmptyCell][yEmptyCell] = board[xMouse][yMouse];
+      board[xMouse][yMouse] = 0;
+      stepsIsDone = true;
+    };
+  };
+  return {
+    board: board,
+    stepsIsDone: stepsIsDone
+  };
+};
+
+// Действия по клику
+const turnGameStep = (e: MouseEvent, ctx: CanvasRenderingContext2D, board: number[][], bg: HTMLImageElement) => {
+  const x = (e.pageX - (e.target as HTMLCanvasElement).offsetLeft) / 200 | 0;
+  const y = (e.pageY - (e.target as HTMLCanvasElement).offsetTop)  / 200 | 0;
+  const newBoard = board && movePuzzleItem(board, x, y);
+  board = newBoard.board;
+  ctx && board && drawField(ctx, board, bg);
+  if (board && isWin(board)) {
+    console.log('You win')
+  }
+  return newBoard.stepsIsDone;
+};
+
+export { drawField, mixBoard, turnGameStep };
--- packages/client/src/pages/GameFieldPage/GameFieldPage.tsx
+++ packages/client/src/pages/GameFieldPage/GameFieldPage.tsx
@@ -1,45 +1,57 @@
 import { useEffect, useRef } from "react";
-import img from '../../assets/bgPuzzle.svg'
+import { drawField, mixBoard, turnGameStep } from "../../Controllers/CanvasController";
+import './style.scss';
+import img from '../../assets/bgPuzzle.svg';
 
 export const GameFieldPage = (): JSX.Element => {
   const ref = useRef<HTMLCanvasElement>(null);
   const bg = new Image()
   bg.src = img
+  const boardNumber: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0];
+  const board = mixBoard(boardNumber);
+  let steps = 0;
 
   useEffect(() => {
-      const ctx = ref.current?.getContext('2d');
-      let cardNumber = 1;
-      bg.onload = function() {
-        for (let i = 0; i < 4; i++) {
-          for (let j = 0; j < 4; j++) {
-            let dx = 0;
-            if (cardNumber > 9) {
-              dx= 40;
-            } else {
-              dx = 0;
-            }
-            if (ctx) {
-              ctx.font = "128px serif";
-              ctx.fillStyle = '#F5F5F5';
-              ctx.fillRect(j * 200, i * 200, 210, 210);
-              if (cardNumber === 16) {
-                ctx.clearRect((5 + i * 200), (5 + j * 200), 200, 200);
-                break;
-              };
-              ctx.drawImage(bg, (5 + j * 200), (5 + i * 200));
-              ctx.fillText(`${cardNumber}`, 65 - dx + 200 * j, 150 + 200 * i);
-              cardNumber++;
-  
-            }
+    const ctx = ref.current?.getContext('2d');
+    bg.addEventListener('load', () => {
+      ctx && board && drawField(ctx, board, bg);
+    });
 
-            
-          }
-        }
+    const canvas = document.querySelector('canvas');
+    const gameSteps = document.querySelector('.game__steps');
+    const gameTime = document.querySelector('.game__time');
+
+    const timeStart = Number(new Date);
+    setInterval(() => {
+      const timeCurrent = Number(new Date);
+      const time = timeCurrent - timeStart;
+      let timeString = `Время: ${Math.round(time / 1000)} сек`;
+      if (time >= 60000) {
+        timeString = `Время: ${Math.floor(time / 60000)} мин ${Math.round((time % 60000) / 1000)} сек`;
       }
-      
-  }, []);
+      if (gameTime) {
+        gameTime.textContent = timeString;
+      };
+    }, 1000)
+
+    ctx && board && canvas?.addEventListener('click', (e) => {
+      const stepsIsDone = turnGameStep(e,ctx,board,bg)
+      if (gameSteps && stepsIsDone) {
+        steps++;
+        gameSteps.textContent = `Ходы: ${steps}`
+      }
+    }), []
+  });
+  
+
 
   return (
-      <canvas ref = {ref} width={825} height={825} />
+    <div className="game">
+      <canvas ref={ref} width={825} height={825} />
+      <div className="game__data-wrapper">
+        <div className="game__steps">Ходы: 0</div>
+        <div className="game__time">Время: 0</div>
+      </div>
+    </div>
   );
 }
