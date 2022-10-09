474e4db2b3fcd230b034697d61cfe27130d443d5
Evgeniy <turkin-e.v@yandex.ru>
2022-10-10 00:13:12 +0400

    fix after review

--- packages/client/src/Controllers/CanvasController.tsx
+++ packages/client/src/Controllers/CanvasController.tsx
@@ -1,30 +1,33 @@
+import { Board } from "./types";
+import { tileSize, tileWithBorder, tileBorder, gameFieldSize, dxText, dyText } from "../const";
+import { useEffect, useState } from "react";
+import img from '../assets/bgPuzzle.svg';
+
 // Отрисовка игрового поля
-const drawField = (ctx: CanvasRenderingContext2D, board: number[][], bg: HTMLImageElement): void => {
-  for (let i = 0; i < 4; i++) {
-    for (let j = 0; j < 4; j++) {
+const drawField = (ctx: CanvasRenderingContext2D, board: Board, bg: HTMLImageElement): void => {
+  for (let i = 0; i < gameFieldSize; i++) {
+    for (let j = 0; j < gameFieldSize; j++) {
       let dx = 0;
-      if (board && board[i][j] > 9) {
+      if (board[i][j] > 9) {
         dx= 20;
       } else {
         dx = 0;
       };
-      if (ctx) {
-        ctx.font = "64px serif";
-        ctx.fillStyle = '#F5F5F5';
-        ctx.fillRect(j * 120, i * 120, 130, 130);
-        if (board && board[i][j] === 0) {
-          ctx.clearRect((5 + j * 120), (5 + i * 120), 120, 120);
-        } else {
-          ctx.drawImage(bg, (5 + j * 120), (5 + i * 120));
-          ctx.fillText(`${board[i][j]}`, 45 - dx + 120 * j, 80 + 120 * i);
-        }
+      ctx.font = "64px serif";
+      ctx.fillStyle = '#F5F5F5';
+      ctx.fillRect(j * tileSize, i * tileSize, tileWithBorder, tileWithBorder);
+      if (board && board[i][j] === 0) {
+        ctx.clearRect((tileBorder + j * tileSize), (tileBorder + i * tileSize), tileSize, tileSize);
+      } else {
+        ctx.drawImage(bg, (tileBorder + j * tileSize), (tileBorder + i * tileSize));
+        ctx.fillText(`${board[i][j]}`, dxText - dx + tileSize * j, dyText + tileSize * i);
       }
     }
   } 
 };
 
 // Получаем позицию пустой ячейки
-const getEmptyCell = (board: number[][]): {xEmptyCell: number, yEmptyCell: number} | undefined => {
+const getEmptyCell = (board: Board): {xEmptyCell: number, yEmptyCell: number} | undefined => {
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[i].length; j++) {
       if (board[j][i] === 0) {
@@ -38,12 +41,13 @@ const getEmptyCell = (board: number[][]): {xEmptyCell: number, yEmptyCell: numbe
 };
 
 // Перемешиваем случайным образом пятнашки на поле
-const mixBoard = (board: number[]): number[][] | undefined => {
+const board: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0];
+const mixBoard = (): Board | undefined => {
   board.sort(() => Math.random() - 0.5);
   const newBoard = [];
-  for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < gameFieldSize; i++) {
     const subArray = []
-    for(let j = i * 4 + 0; j < (i + 1) * 4; j++) {
+    for(let j = i * gameFieldSize; j < (i + 1) * gameFieldSize; j++) {
       subArray.push(board[j]);
     }
     newBoard.push(subArray);
@@ -52,7 +56,7 @@ const mixBoard = (board: number[]): number[][] | undefined => {
 };
 
 // Проверка победы
-const isWin = (board: number[][]): boolean => {
+const isWin = (board: Board): boolean => {
   const winBoard = [
     [1, 2, 3, 4],
     [5, 6, 7, 8],
@@ -71,7 +75,7 @@ const isWin = (board: number[][]): boolean => {
 };
 
 // Перемещаем пятнашку на пустое место
-const movePuzzleItem = (board: number[][], yMouse: number, xMouse: number): {board: number[][], stepsIsDone: boolean} => {
+const movePuzzleItem = (board: Board, yMouse: number, xMouse: number): {board: Board, stepsIsDone: boolean} => {
   const emptyCell = getEmptyCell(board);
   let stepsIsDone = false;
   if (emptyCell) {
@@ -94,7 +98,7 @@ const movePuzzleItem = (board: number[][], yMouse: number, xMouse: number): {boa
 };
 
 // Действия по клику
-const turnGameStep = (e: MouseEvent, ctx: CanvasRenderingContext2D, board: number[][], bg: HTMLImageElement) => {
+const turnGameStep = (e: MouseEvent, ctx: CanvasRenderingContext2D, board: Board, bg: HTMLImageElement): boolean => {
   const x = (e.pageX - (e.target as HTMLCanvasElement).offsetLeft) / 120 | 0;
   const y = (e.pageY - (e.target as HTMLCanvasElement).offsetTop)  / 120 | 0;
   const newBoard = board && movePuzzleItem(board, x, y);
@@ -108,4 +112,34 @@ const turnGameStep = (e: MouseEvent, ctx: CanvasRenderingContext2D, board: numbe
   return newBoard.stepsIsDone;
 };
 
-export { drawField, mixBoard, turnGameStep };
+const init = (ref: React.RefObject<HTMLCanvasElement>, refSteps: React.RefObject<HTMLDivElement>) => {
+  const bg = new Image()
+  const [board] = useState(mixBoard())
+  bg.src = img
+  let steps = 0;
+  useEffect(() => {
+    const ctx = ref.current?.getContext('2d');
+    bg.addEventListener('load', () => {
+      ctx && board && drawField(ctx, board, bg);
+    });
+  },[])
+
+  useEffect(() => {
+    const ctx = ref.current?.getContext('2d');
+    const canvas = ref.current;
+    const handleCLick = (e: MouseEvent) => {
+      const stepsIsDone = ctx && board && turnGameStep(e,ctx,board,bg)
+      if (refSteps.current && stepsIsDone) {
+        steps++;
+        refSteps.current.textContent = `Ходы: ${steps}`;
+      }
+    }
+    canvas?.addEventListener('click', (e) => handleCLick(e));
+
+    return () => {
+      canvas?.removeEventListener('click', (e) => handleCLick(e));
+    }
+  }, [steps])
+}
+
+export { init }
--- packages/client/src/pages/GameFieldPage/GameFieldPage.tsx
+++ packages/client/src/pages/GameFieldPage/GameFieldPage.tsx
@@ -1,41 +1,12 @@
-import { useEffect, useRef, useState } from "react";
-import { drawField, mixBoard, turnGameStep } from "../../Controllers/CanvasController";
+import { useRef } from "react";
 import { Timer } from "../../components/ui/Timer";
-import './style.scss';
-import img from '../../assets/bgPuzzle.svg';
+import { init } from "../../Controllers/CanvasController";
+import "./style.scss";
 
 export const GameFieldPage = (): JSX.Element => {
   const ref = useRef<HTMLCanvasElement>(null);
   const refSteps = useRef<HTMLDivElement>(null);
-  const bg = new Image()
-  const boardNumber: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0];
-  const [board] = useState(mixBoard(boardNumber))
-  bg.src = img
-  let steps = 0;
-  useEffect(() => {
-    const ctx = ref.current?.getContext('2d');
-    bg.addEventListener('load', () => {
-      ctx && board && drawField(ctx, board, bg);
-    });
-  },[])
-
-  useEffect(() => {
-    const ctx = ref.current?.getContext('2d');
-    const canvas = ref.current;
-    const handleCLick = (e: MouseEvent) => {
-      const stepsIsDone = ctx && board && turnGameStep(e,ctx,board,bg)
-      if (refSteps.current && stepsIsDone) {
-        steps++;
-        refSteps.current.textContent = `Ходы: ${steps}`;
-      }
-    }
-    canvas?.addEventListener('click', (e) => handleCLick(e));
-
-    return () => {
-      canvas?.removeEventListener('click', (e) => handleCLick(e));
-    }
-  }, [steps])
-  
+  init(ref, refSteps);
 
   return (
     <div className="game">
