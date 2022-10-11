f7873dc5e007083198dde8b0615dfe153972e108
RoRux <rorux84@gmail.com>
2022-10-11 22:02:35 +0300

    refactor: change useTimer hook

--- packages/client/src/hooks/useTimer.tsx
+++ packages/client/src/hooks/useTimer.tsx
@@ -1,27 +1,12 @@
 import { useState, useEffect } from "react";
 
-const useTimer = (ref: any) => {
-
-  const [timeStart] = useState(Number(new Date));
-  const [time, setTime] = useState(0);
+export const useTimer = () => {
+  const [counter, setCounter] = useState(0);
 
   useEffect(() => {
-    const gameTime = ref.current;
-    const timer = setInterval(() => {
-      const timeCurrent = Number(new Date);
-      setTime(timeCurrent - timeStart);
-      let timeString = `Время: ${Math.round(time / 1000)} сек`;
-      if (time >= 60000) {
-        timeString = `Время: ${Math.floor(time / 60000)} мин ${Math.round((time % 60000) / 1000)} сек`;
-      }
-      if (gameTime) {
-        gameTime.textContent = timeString;
-      };
-    }, 1000);
-    return () => {
-      clearInterval(timer);
-    }
-  }, [time]);
-}
+    const timer = setInterval(() => setCounter(counter + 1), 1000);
+    return () => clearInterval(timer);
+  }, [counter]);
 
-export { useTimer }
+  return { counter }
+}
