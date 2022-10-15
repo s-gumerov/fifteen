facabe1adb968d120bf5e1a37801a2b4d0dd28f8
RoRux <rorux84@gmail.com>
2022-10-15 14:15:45 +0300

    refactor game logic

--- packages/client/src/pages/GameFieldPage/GameFieldPage.tsx
+++ packages/client/src/pages/GameFieldPage/GameFieldPage.tsx
@@ -1,21 +1,46 @@
-import { useRef } from "react";
-import { init } from "../../Controllers/CanvasController";
-import { useTimer } from '../../hooks/useTimer';
+import { useEffect, useRef, useState } from 'react'
+import { isEqual } from 'lodash'
 import { transformSecondsCountToWordExpression } from '../../utils';
+import { CanvasController } from '../../Controllers';
+import { useTimer } from '../../hooks/useTimer';
+import { TBoard } from '../../Controllers/CanvasController/types';
 import "./style.scss";
 
+const Canvas = new CanvasController;
+
 export const GameFieldPage = (): JSX.Element => {
-  const ref = useRef<HTMLCanvasElement>(null);
-  const refSteps = useRef<HTMLDivElement>(null);
-  const { counter } = useTimer();
-  init(ref, refSteps);
+  const [board, setBoard] = useState(Canvas.mixBoard());
+  const [stepsCount, setStepsCount] = useState(0);
+  const fieldRef = useRef<HTMLCanvasElement>(null);
+  const { secondsCount, toggleSecondsCounter, setToggleSecondsCounter } = useTimer();
+
+  const backgroundPuzzle = Canvas.getBackgroundPuzzle();
+
+  const handleClickOnField = (event: React.MouseEvent) => {
+    const previousBoard = [[...board[0]], [...board[1]], [...board[2]], [...board[3]]];
+    const currentBoard = Canvas.getBoardAfterClick(event, previousBoard);
+
+    if(!isEqual(board, currentBoard)) {
+      setStepsCount(stepsCount + 1);
+      setBoard(currentBoard);
+      Canvas.drawField(fieldRef, currentBoard, backgroundPuzzle);
+
+      if(Canvas.isWin(currentBoard)) {
+        console.log('You win')
+      }
+    }
+  }
+
+  useEffect(() => {
+    Canvas.init(fieldRef, board as TBoard, backgroundPuzzle);
+  }, []);
 
   return (
     <div className="game">
-      <canvas ref={ref} width={505} height={505} />
+      <canvas ref={fieldRef} onClick={handleClickOnField} width={505} height={505} />
       <div className="game__data-wrapper">
-        <div ref={refSteps} className="game__steps">Ходы: 0</div>
-        <div>Время: {transformSecondsCountToWordExpression(counter)}</div>
+        <div className="game__steps">Ходы: {stepsCount}</div>
+        <div>Время: {transformSecondsCountToWordExpression(secondsCount)}</div>
       </div>
     </div>
   );
