9e4d7226e4f780cdaf74dc93a351aa34aa0a0670
Evgeniy <turkin-e.v@yandex.ru>
2022-10-16 23:00:52 +0400

    add sw and all urls

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -9,7 +9,7 @@ import './styles.scss';
 function startServiceWorker() {
   if ("serviceWorker" in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register("/serviceWorker.js").then(registration => {
+      navigator.serviceWorker.register("/serviceWorker.ts").then(registration => {
         console.log("ServiceWorker registration successful with  scope: ", registration.scope);
       }).catch((error: string) => {
         console.log("ServiceWorker registration failed: ", error);
