e8f4cdf95ba400178388c27659d90725c8150e14
Evgeniy Turkin <97192795+Evgeniy0805@users.noreply.github.com>
2022-10-18 09:27:32 +0400

    Features/gam 30 (#15)
    * add service worker

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -6,8 +6,20 @@ import { withErrorBoundary } from 'react-error-boundary';
 import { ErrorFallback } from './services/errorBoundary/ErrorFallback';
 import './styles.scss';
 
+function startServiceWorker() {
+  if ("serviceWorker" in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register("/serviceWorker.ts").then(registration => {
+        console.log("ServiceWorker registration successful with  scope: ", registration.scope);
+      }).catch((error: string) => {
+        console.log("ServiceWorker registration failed: ", error);
+      });
+    })
+  }
+}
 
 function App() {
+  startServiceWorker(); 
   // useEffect(() => {
   //   const fetchServerData = async () => {
   //     const url = `http://localhost:${__SERVER_PORT__}`
