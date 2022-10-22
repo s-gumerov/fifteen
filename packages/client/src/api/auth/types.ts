b813611bf8829a0be56dedfd0bbea114d172f970
sandyrev igor <sandyrev.igor@gmail.com>
2022-10-22 17:23:08 +0500

    GAM-31 Added profile API with createAsyncThunk

--- packages/client/src/api/auth/types.ts
+++ packages/client/src/api/auth/types.ts
@@ -7,15 +7,24 @@ export type TSignupData = {
   first_name: string,
   second_name: string,
   email: string,
-  phone: string
+  phone: string,
   password_again?: string,
 } & TAuthData;
 
 export type TUserInfo = {
-  id: number,
+  id?: number,
   display_name: string,
-  avatar: string,
+  avatar?: string,
 } & Omit<TSignupData, 'password'>;
+
+export type TChangeAvatar = {
+  avatar: FormData;
+}
+
+export type TUserPassword = {
+  oldPassword: string,
+  newPassword: string
+}
 
 export type TBadRequest = {
   reason: string
@@ -26,3 +35,6 @@ export type TSignupResponse = {
 } | undefined;
 
 export type TAuthResponse = "OK" | undefined;
+export type TChangeProfileResponse = "OK" | undefined;
+export type TChangePasswordResponse = "OK" | undefined;
+export type TChangeAvatarResponse = "OK" | undefined;
