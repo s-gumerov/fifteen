2a0a045e173f03cdae9d5fc4cc1f3daf3a362fcd
sandyrev igor <sandyrev.igor@gmail.com>
2022-11-06 23:11:33 +0500

    Added yandex OAuth

--- packages/client/src/api/const.ts
+++ packages/client/src/api/const.ts
@@ -1,18 +1,20 @@
 export const BASE_URL_API = 'https://ya-praktikum.tech';
-
+export const REDIRECT_URI = `http://${location.host}`
+// export const REDIRECT_URI = 'http://localhost:3000'
 export enum HTTP_STATUS_CODE {
   OK = 200,
   BAD_REQUEST = 400,
   UNAUTHORIZED = 401,
   INTERNAL_SERVER_ERROR = 500,
-};
+}
 
 export enum STORE_NAME {
   USER = 'fifteenUserStore',
   LEADERBOARD  = "fifteenLeaderboardStore",
-};
+}
 
 export enum ENDPOINT{
   LEADERBOARD = '/api/v2/leaderboard',
   ALLLEADERBOARD  = '/api/v2/leaderboard/all',
-};
+}
+
