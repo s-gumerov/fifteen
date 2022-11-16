c76a3d31c29ec6b42652a65e73e70f901f41c5cf
Salavat <s.gumerov.work@gmail.com>
2022-11-16 23:35:19 +0500

    Recreation of the client Redux storage from the server is configured

--- webpack/src/client/index.tsx
+++ webpack/src/client/index.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
 import App from "./App";
 import store from './store';
+import {AnyAction, Store} from '@reduxjs/toolkit';
 
 
 declare global {
     interface Window {
-        __PRELOADED_STATE__: any;
+        __PRELOADED_STATE__?: Store<any, AnyAction>;
     }
 }
-const state = window.__PRELOADED_STATE__ ;
+
+const serverStore = window.__PRELOADED_STATE__;
 
 delete window.__PRELOADED_STATE__;
-console.log(state)
+console.log(serverStore);
 
 ReactDOM.hydrateRoot(
-  document.getElementById('root') as HTMLElement,
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-   </Provider>
+    document.getElementById('root') as HTMLElement,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
 )
