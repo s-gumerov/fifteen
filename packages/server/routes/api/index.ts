6572194fdca0cf5aac29b437fa1faba93eedde9a
turkinev <turkinev@63pokupki.ru>
2022-11-25 18:50:52 +0400

    add /api

--- packages/server/routes/api/index.ts
+++ packages/server/routes/api/index.ts
@@ -3,12 +3,13 @@ import userRouter from "./users"
 import topicRouter from "./topics"
 import threadRouter from "./threads"
 import threadAnswerRouter from "./threadAnswers"
+import { API_URL } from "../../const";
 
 const router = Router();
 
-router.use('/', userRouter)
-router.use('/', topicRouter)
-router.use('/', threadRouter)
-router.use('/', threadAnswerRouter)
+router.use(API_URL, userRouter)
+router.use(API_URL, topicRouter)
+router.use(API_URL, threadRouter)
+router.use(API_URL, threadAnswerRouter)
 
 export { router }
