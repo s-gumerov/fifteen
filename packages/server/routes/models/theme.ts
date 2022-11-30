3c061d4a0bf436e107be7f9d76628a8169ce3f91
Evgeniy Turkin <97192795+Evgeniy0805@users.noreply.github.com>
2022-11-30 21:01:18 +0400

    update (#31)
    Co-authored-by: turkinev <turkinev@63pokupki.ru>

---
+++ packages/server/routes/models/theme.ts
@@ -0,0 +1,18 @@
+/**
+ * Получить тему
+ */
+export namespace getTheme {
+    /** APIURL */
+    export const route = '/get-theme'
+
+    /** Параметры api запроса */
+    export interface Request {
+        themeName: string
+    }
+
+    /** Параметры api ответа */
+    export interface Response {
+        mainColor: string
+        secondColor: string
+    }
+}
